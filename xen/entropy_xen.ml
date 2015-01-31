a1dca0a4cef53405860852393eeac32f5bdc2f80
David Scott <dave.scott@eu.citrix.com>
2015-01-31 12:09:04 +0000

    xen: periodically refeed entropy, the same as the Unix implementation
    It's now a fatal error to fail to read the host entropy device, if one has
    been configured.
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- xen/entropy_xen.ml
+++ xen/entropy_xen.ml
@@ -27,6 +27,9 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *)
 
+let period = 600. (* refeed every <period> seconds *)
+let chunk = 16    (* read <chunk> bytes of entropy every <period> *)
+
 open Lwt
 
 type 'a io  = 'a Lwt.t
@@ -37,11 +40,16 @@ type handler = source:int -> buffer -> unit
 
 type id = [ `FromHost | `Weak ]
 
-type t =
+type implementation =
 | RandomSelfInit (* Implementation of `Weak *)
 | EntropyConsole of Console_xen.t (* Implementation of `FromHost *)
 
-let id = function
+type t = {
+  implementation: implementation;
+  mutable ev: Lwt_engine.event option;
+}
+
+let id t = match t.implementation with
 | RandomSelfInit -> `Weak
 | EntropyConsole _ -> `FromHost
 
@@ -70,7 +78,7 @@ let connect = function
 | `Weak ->
   Random.self_init ();
   print_endline "Entropy_xen: using a weak entropy source seeded only from time.";
-  return (`Ok RandomSelfInit)
+  return (`Ok { implementation = RandomSelfInit; ev = None })
 | `FromHost ->
   Printf.printf "Entropy_xen: attempting to connect to Xen entropy source %s\n%!" console_name;
   Console_xen.connect console_name
@@ -86,19 +94,24 @@ let connect = function
     return (`Error (`No_entropy_device console_name))
   end else begin
     print_endline "Entropy_xen: connected to Xen entropy source";
-    return (`Ok (EntropyConsole device))
+    return (`Ok { implementation = EntropyConsole device; ev = None })
   end
 
 let disconnect _ = return_unit
 
-let chunk = 16
-
-let refeed t f = match t with
+let refeed t f = match t.implementation with
 | EntropyConsole console ->
-  Console_xen.read console
-  >>|= fun cs ->
+  ( Console_xen.read console
+    >>= function
+    | `Ok cs ->
+      return cs
+    | _ ->
+      Printf.printf "ERROR: reading from entropy device. We have no more entropy\n%!";
+      (* Abort the unikernel *)
+      failwith "Unable to read from entropy device"
+  ) >>= fun cs ->
   f ~source:(Cstruct.get_uint8 cs 0) (Cstruct.shift cs 1);
-  return (`Ok ())
+  return_unit
 | RandomSelfInit ->
   let s  = Random.int 256
   and cs = Cstruct.create chunk in
@@ -106,11 +119,29 @@ let refeed t f = match t with
     Cstruct.set_uint8 cs i Random.(int 256)
   done ;
   f ~source:s cs;
-  return (`Ok ())
+  return_unit
+
+let stop t =
+  match t.ev with
+  | None    -> ()
+  | Some ev -> Lwt_engine.stop_event ev ; t.ev <- None
+
+(*
+ * Registering a `handler` spins up a recurrent timer.
+ *
+ * XXX There should be no timer in the first place; `refeed` should piggyback
+ * on other activity going on in the system. We should trigger refeeding every
+ * time the engine fires some _other_ callback, throttling the frequency. That
+ * way, refeeding would be broadly aligned with the general activity of the
+ * system instead of forcing the `period`.
+ *)
 
 let handler t f : unit Lwt.t =
-  (* Read data from the console and advertise it.
-     FIXME: we should do this more than once. How often? *)
-  let (_: 'a Lwt.t) = refeed t f in
+  let trigger _ = async (fun () -> refeed t f) in
+  stop t;
+  refeed t f
+  >>= fun () ->
+  let ev = Lwt_engine.on_timer period true trigger in
+  t.ev <- Some ev;
   return_unit
 
