da21fc3bfa32f637a1700e09bd9fa0c83fb63251
pqwy <david@numm.org>
2015-02-06 03:14:58 +0000

    remove separate mls

--- xen/entropy_cpu_xen.mli
+++
@@ -1,6 +0,0 @@
-exception Instruction_unavailable of string
-exception Entropy_unavailable
-
-val rdtscp : unit -> int = "caml_rdtscp" "noalloc"
-val rdrand : unit -> int = "caml_rdrand"
-val rdseed : unit -> int = "caml_rdseed"
