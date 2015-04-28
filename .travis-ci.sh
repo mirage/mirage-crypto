06a95ce42d963361d68b0a7074be2493959a2fb2
Hannes Mehnert <hannes@mehnert.org>
2015-04-28 11:17:28 +0100

    also need fresh binutils

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,7 +2,7 @@
 
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
-sudo apt-get -qq install gcc-4.8
+sudo apt-get -qq install gcc-4.8 binutils
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
 
 bash -ex .travis-opam.sh
