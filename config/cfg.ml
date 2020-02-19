7e489afbd922d55719c3f54fceb7f334b95c9861
Hannes Mehnert <hannes@mehnert.org>
2020-02-19 21:58:31 +0100

    preserve CFLAGS across targets by centralising default cflags in config/cfg.ml

--- config/cfg.ml
+++ config/cfg.ml
@@ -1,12 +1,15 @@
 let evar  = "MIRAGE_CRYPTO_ACCELERATE"
 let needs = [`SSSE3; `AES; `PCLMULQDQ]
 let flags = ["-DACCELERATE"; "-mssse3"; "-maes"; "-mpclmul"]
+let std_flags = ["--std=c99"; "-Wall"; "-Wextra"; "-O3"]
 
 let _ =
   let auto = match Cpuid.supports needs with Ok true -> flags | _ -> [] in
-  let fs = match Sys.getenv evar with
-    "true" -> flags
-  | "false" -> []
-  | _ -> auto
-  | exception Not_found -> auto in
-  Format.(printf "(@[%a@])%!" (fun ppf -> List.iter (fprintf ppf "%s@ ")) fs)
+  let accelerate_flags = match Sys.getenv evar with
+    | "true" -> flags
+    | "false" -> []
+    | _ -> auto
+    | exception Not_found -> auto
+  in
+  let fs = std_flags @ accelerate_flags in
+  Format.(printf "(@[%a@])@.%!" (fun ppf -> List.iter (fprintf ppf "%s@ ")) fs)
