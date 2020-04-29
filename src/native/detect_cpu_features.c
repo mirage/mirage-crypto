1b6d08ac2c8fcc405cfcf0d0a042c26f7246fc58
Hannes Mehnert <hannes@mehnert.org>
2020-04-29 23:14:37 +0200

    avoid link error on travis (+mirage-xen)

--- src/native/detect_cpu_features.c
+++ src/native/detect_cpu_features.c
@@ -8,20 +8,24 @@ struct _mc_cpu_features mc_detected_cpu_features = { 0 };
 
 CAMLprim value
 mc_detect_cpu_features (__unit ()) {
-  unsigned int eax = 0, ebx = 0, ecx = 0, edx = 0;
+  unsigned int sig = 0, eax = 0, ebx = 0, ecx = 0, edx = 0;
 
-  if (__get_cpuid(1, &eax, &ebx, &ecx, &edx)) {
-    if (ecx & bit_PCLMUL)
-      mc_detected_cpu_features.pclmul = 1;
-    if (ecx & bit_SSSE3)
-      mc_detected_cpu_features.ssse3 = 1;
-    if (ecx & bit_AES)
-      mc_detected_cpu_features.aesni = 1;
-    if (ecx & bit_RDRND)
-      mc_detected_cpu_features.rdrand = 1;
-  }
+  int max = __get_cpuid_max(0, &sig);
 
-  if (__get_cpuid_count(7, 0, &eax, &ebx, &ecx, &edx)) {
+  if (max < 1) return Val_unit;
+
+  __cpuid(1, eax, ebx, ecx, edx);
+  if (ecx & bit_PCLMUL)
+    mc_detected_cpu_features.pclmul = 1;
+  if (ecx & bit_SSSE3)
+    mc_detected_cpu_features.ssse3 = 1;
+  if (ecx & bit_AES)
+    mc_detected_cpu_features.aesni = 1;
+  if (ecx & bit_RDRND)
+    mc_detected_cpu_features.rdrand = 1;
+
+  if (max > 7) {
+    __cpuid_count(7, 0, eax, ebx, ecx, edx);
     if (ebx & bit_RDSEED)
       mc_detected_cpu_features.rdseed = 1;
   }
