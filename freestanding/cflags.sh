f3e5dd6371e211e4b0292d92c325a450ae57b014
Hannes Mehnert <hannes@mehnert.org>
2020-05-11 01:14:47 +0200

    support for compiling without opam (where PKG_CONFIG_PATH is already set)

--- freestanding/cflags.sh
+++ freestanding/cflags.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-export PKG_CONFIG_PATH="$(opam config var lib)/pkgconfig"
+
+pkg_exists() {
+    pkg-config --exists "ocaml-freestanding"
+}
+if ! pkg_exists; then
+    export PKG_CONFIG_PATH="$(opam config var lib)/pkgconfig"
+fi
+pkg_exists || exit 1
+
 flags="$(pkg-config --static ocaml-freestanding --cflags)"
 echo "($flags)"
