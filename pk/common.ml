86fb994ddcc2648f84d8adf1aae35da55315cbe5
Hannes Mehnert <hannes@mehnert.org>
2021-10-21 22:40:56 +0200

    use whitespaces around let ( let* ) (code review by @reynir)

--- pk/common.ml
+++ pk/common.ml
@@ -2,7 +2,7 @@ let rec until p f = let r = f () in if p r then r else until p f
 
 let guard p err = if p then Ok () else Error err
 
-let (let*) = Result.bind
+let ( let* ) = Result.bind
 
 (* The Sexplib hack... *)
 module Z_sexp = struct
