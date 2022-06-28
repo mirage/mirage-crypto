5b8ab138807fe5a2cca451e829e7454ff5567cb1
Bikal Lem <gbikal@gmail.com>
2022-06-28 14:16:32 +0100

    eio(tests) : add entropy collection test

--- eio/tests/test_eio_rng.ml
+++ eio/tests/test_eio_rng.ml
@@ -1,6 +1,8 @@
+open Mirage_crypto_rng
+
 let () = 
   Eio_main.run @@ fun env -> 
-  Mirage_crypto_rng_eio.run env @@ fun () ->
+   Mirage_crypto_rng_eio.run (module Fortuna) env @@ fun () ->
   let random_num = Mirage_crypto_rng.generate 32 in 
   assert (Cstruct.length random_num = 32);
   Printf.printf "32 bit random number: %S%!" (Cstruct.to_string random_num);
