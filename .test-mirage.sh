9fa3f56ed9ab849cfa7457cf0d7cdf3caaaf526d
Hannes Mehnert <hannes@mehnert.org>
2022-09-13 15:22:38 +0200

    test-mirage: run mirage from top directory

--- .test-mirage.sh
+++ .test-mirage.sh
@@ -3,8 +3,9 @@
 set -ex
 
 opam install --confirm-level=unsafe-yes "mirage>4"
-(cd mirage && mirage configure -t unix && gmake depend && mirage build && dist/crypto-test && cd ..) || exit 1
-(cd mirage && mirage configure -t hvt && gmake depend && mirage build && cd ..) || exit 1
+dune subst
+(mirage configure -t unix -f mirage/config.ml && gmake depend && mirage build -f mirage/config.ml && mirage/dist/crypto-test) || exit 1
+(mirage configure -t hvt -f mirage/config.ml && gmake depend && mirage build -f mirage/config.ml) || exit 1
 if [ $(uname -m) = "amd64" ] || [ $(uname -m) = "x86_64" ]; then
-    (cd mirage && mirage configure -t xen && gmake depend && mirage build && cd ..) || exit 1
+    (mirage configure -t xen -f mirage/config.ml && gmake depend && mirage build -f mirage/config.ml) || exit 1
 fi
