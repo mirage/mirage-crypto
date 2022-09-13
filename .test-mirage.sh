879cc340db62319ab019992a2536b24e4cd44554
Hannes Mehnert <hannes@mehnert.org>
2022-09-13 13:10:25 +0200

    cirrus: use mirage4, bump ocaml versions

--- .test-mirage.sh
+++ .test-mirage.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-opam install -y "mirage<4"
-(cd mirage && mirage configure -t unix && make depend && mirage build && ./crypto_test && mirage clean && cd ..) || exit 1
-(cd mirage && mirage configure -t hvt && make depend && mirage build && mirage clean && cd ..) || exit 1
+opam install --confirm-level=unsafe-yes "mirage>4"
+(cd mirage && mirage configure -t unix && gmake depend && mirage build && dist/crypto-test && cd ..) || exit 1
+(cd mirage && mirage configure -t hvt && gmake depend && mirage build && cd ..) || exit 1
 if [ $(uname -m) = "amd64" ] || [ $(uname -m) = "x86_64" ]; then
-    (cd mirage && mirage configure -t xen && make depend && mirage build && mirage clean && cd ..) || exit 1
+    (cd mirage && mirage configure -t xen && gmake depend && mirage build && cd ..) || exit 1
 fi
