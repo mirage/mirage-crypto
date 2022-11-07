c2ddf982f12e76b0254b806d71474df74495b59a
Hannes Mehnert <hannes@mehnert.org>
2022-11-07 13:14:02 +0100

    Ci use mirage4 (#166)
    * cirrus: use mirage4, bump ocaml versions
    
    * test-mirage: run mirage from top directory
    
    * test-mirage: add specific version to opam file
    
    * @TheLortex suggests to only build mirage/dist
    
    * *opam: raise dune lower bound to 2.7 (for mirage CI)
    
    * cirrus: eio use beta1

--- .test-mirage.sh
+++ .test-mirage.sh
@@ -2,9 +2,16 @@
 
 set -ex
 
-opam install -y "mirage<4"
-(cd mirage && mirage configure -t unix && make depend && mirage build && ./crypto_test && mirage clean && cd ..) || exit 1
-(cd mirage && mirage configure -t hvt && make depend && mirage build && mirage clean && cd ..) || exit 1
+opam install --confirm-level=unsafe-yes "mirage>4"
+# to satisfy hardcoded version constraints in mirage, we need to be < 0.11.0
+# and "dune subst" doesn't work on these PR checkouts
+version='version: "0.10.99~dev"'
+echo $version >> mirage-crypto-rng-mirage.opam
+echo $version >> mirage-crypto-rng.opam
+echo $version >> mirage-crypto.opam
+echo $version >> mirage-crypto-pk.opam
+(mirage configure -t unix -f mirage/config.ml && gmake depend && dune build --root . mirage/dist/ && mirage/dist/crypto-test) || exit 1
+(mirage configure -t hvt -f mirage/config.ml && gmake depend && dune build --root . mirage/dist/) || exit 1
 if [ $(uname -m) = "amd64" ] || [ $(uname -m) = "x86_64" ]; then
-    (cd mirage && mirage configure -t xen && make depend && mirage build && mirage clean && cd ..) || exit 1
+    (mirage configure -t xen -f mirage/config.ml && gmake depend && dune build --root . mirage/dist/) || exit 1
 fi
