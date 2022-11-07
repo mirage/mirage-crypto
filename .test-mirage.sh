d5547d3b596c5206c93d2ed085633387f8b78123
Hannes Mehnert <hannes@mehnert.org>
2022-11-07 11:15:23 +0100

    @TheLortex suggests to only build mirage/dist

--- .test-mirage.sh
+++ .test-mirage.sh
@@ -10,8 +10,8 @@ echo $version >> mirage-crypto-rng-mirage.opam
 echo $version >> mirage-crypto-rng.opam
 echo $version >> mirage-crypto.opam
 echo $version >> mirage-crypto-pk.opam
-(mirage configure -t unix -f mirage/config.ml && gmake depend && mirage build -f mirage/config.ml && mirage/dist/crypto-test) || exit 1
-(mirage configure -t hvt -f mirage/config.ml && gmake depend && mirage build -f mirage/config.ml) || exit 1
+(mirage configure -t unix -f mirage/config.ml && gmake depend && dune build --root . mirage/dist/ && mirage/dist/crypto-test) || exit 1
+(mirage configure -t hvt -f mirage/config.ml && gmake depend && dune build --root . mirage/dist/) || exit 1
 if [ $(uname -m) = "amd64" ] || [ $(uname -m) = "x86_64" ]; then
-    (mirage configure -t xen -f mirage/config.ml && gmake depend && mirage build -f mirage/config.ml) || exit 1
+    (mirage configure -t xen -f mirage/config.ml && gmake depend && dune build --root . mirage/dist/) || exit 1
 fi
