2bddd00d399e776d9f4406db6b5997e9985f7baa
Hannes Mehnert <hannes@mehnert.org>
2023-09-18 17:46:27 +0200

    on macos (arm64), avoid -O3 due to miscompilations (#185)
    * on macos (arm64), avoid instcombine due to miscompilation
    
    Co-authored-by: Kate <kit.ty.kate@disroot.org>

--- config/cfg.ml
+++ config/cfg.ml
@@ -8,13 +8,25 @@ let () =
         c
         ~includes:[]
         [("__x86_64__", Switch); ("__i386__", Switch); ("__powerpc64__", Switch);
-         ("__s390x__", Switch)]
+         ("__s390x__", Switch); ("__aarch64__", Switch)]
     in
     match defines with
     | (_, Switch true) :: _ -> `x86_64
     | _ :: (_, Switch true) :: _ -> `x86
     | _ :: _ :: (_, Switch true) :: _ -> `ppc64
     | _ :: _ :: _ :: (_, Switch true) :: _ -> `s390x
+    | _ :: _ :: _ :: _ :: (_, Switch true) :: _ -> `arm64
+    | _ -> `unknown
+  in
+  let os =
+    let defines =
+      Configurator.V1.C_define.import
+        c
+        ~includes:[]
+        [("__APPLE__", Switch)]
+    in
+    match defines with
+    | (_, Switch true) :: _ -> `macos
     | _ -> `unknown
   in
   let accelerate_flags =
@@ -33,7 +45,17 @@ let () =
     | `ppc64 | `s390x -> [ "-Wno-stringop-overflow"; "-Werror" ]
     | _ -> [ "-Werror" ]
   in
-  let flags = std_flags @ ent_flags in
+  let no_instcombine_on_macos = match arch, os with
+    | `arm64, `macos ->
+      let res = Configurator.V1.Process.run c "cc" ["-dumpversion"] in
+      if String.trim res.stdout = "14.0.3" then
+        ["-mllvm"; "--instcombine-max-iterations=0"]
+        (* macOS instcombine miscompilation with clang 14.0.3 *)
+      else
+        []
+    | _ -> []
+  in
+  let flags = std_flags @ no_instcombine_on_macos @ ent_flags in
   let opt_flags = flags @ accelerate_flags in
   Configurator.V1.Flags.write_sexp "cflags_optimized.sexp" opt_flags;
   Configurator.V1.Flags.write_sexp "cflags.sexp" flags;
