84cfdd2543f9047c45ced265a1c8d1e695390db7
Hannes Mehnert <hannes@mehnert.org>
2024-10-01 10:05:40 +0200

    fix mirage example

--- .test-mirage.sh
+++ .test-mirage.sh
@@ -5,12 +5,11 @@ set -ex
 opam install --confirm-level=unsafe-yes "mirage>4"
 # to satisfy hardcoded version constraints in mirage, we need to be < 0.12.0
 # and "dune subst" doesn't work on these PR checkouts
-version='version: "0.11.99~dev"'
+version='version: "1.99.0~dev"'
 echo $version >> mirage-crypto-rng-mirage.opam
 echo $version >> mirage-crypto-rng.opam
 echo $version >> mirage-crypto.opam
 echo $version >> mirage-crypto-pk.opam
-opam pin add -yn mirage-random.3.99 --dev
 (mirage configure -t unix -f mirage/config.ml && gmake depend && dune build --root . mirage/dist/ && mirage/dist/crypto-test) || exit 1
 (mirage configure -t hvt -f mirage/config.ml && gmake depend && dune build --root . mirage/dist/) || exit 1
 if [ $(uname -m) = "amd64" ] || [ $(uname -m) = "x86_64" ]; then
