aa4627ab8f0800e8ce7e7b51f726ce3fdf56e4c3
Hannes Mehnert <hannes@mehnert.org>
2025-01-07 13:45:42 +0100

    Update rng/unix/getentropy.ml
    Co-authored-by: Török Edwin <edwintorok@users.noreply.github.com>

--- rng/unix/getentropy.ml
+++ rng/unix/getentropy.ml
@@ -3,6 +3,11 @@ external getrandom_buf : bytes -> int -> int -> unit = "mc_getrandom" [@@noalloc
 
 type g = unit
 
+(* The maximum value for length is GETENTROPY_MAX for `getentropy`: https://pubs.opengroup.org/onlinepubs/9799919799/functions/getentropy.html
+    The minimum acceptable value for GETENTROPY_MAX is 256 https://pubs.opengroup.org/onlinepubs/9799919799/basedefs/limits.h.html
+    
+    The actual implementation may be one of `getrandom`, `getentropy`, or `BCryptGenRandom`, and will internally limit the maximum bytes read in one go and loop as needed if more bytes are requested and we get a short read.
+  *)
 let block = 256
 
 let create ?time:_ () = ()
