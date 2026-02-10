d61b2a4668fa8360aa165a92696e06b9c9006337
Calascibetta Romain <romain.calascibetta@gmail.com>
2026-02-10 15:34:39 +0100

    Apply suggestions from code review
    Co-authored-by: Hannes Mehnert <hannes@mehnert.org>

--- rng/miou/mirage_crypto_rng_mkernel.mli
+++ rng/miou/mirage_crypto_rng_mkernel.mli
@@ -1,7 +1,12 @@
-(** {b RNG} seeding on {b Miou_solo5}. *)
+(** {b RNG} seeding on {b Mkernel}. *)
 
 type rng
 (** Type of tasks seeding the RNG. *)
 
 val initialize : ?g:'a -> ?sleep:int -> 'a Mirage_crypto_rng.generator -> rng
+(** [initialize ~g ~sleep generator] sets the default generator to the [generator] and sets up periodic entropy feeding for that rng. The argument [sleep] (default: 1 second) is measured in nanoseconds, and is the wait between two CPU-assisted entropy collection (using RDRAND).
+
+Raises [Invalid_argument] if called a second time.
+
 val kill : rng -> unit
+(** [kill rng] cancels the periodic seeding and unsets the default random number generator. *)
