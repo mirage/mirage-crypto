73d070e80d188f0b33d08599a861d0abbbac2ef5
Hannes Mehnert <hannes@mehnert.org>
2026-08-25 14:42:37 +0200

    simplify

--- rng/unix/urandom.ml
+++ rng/unix/urandom.ml
@@ -1,4 +1,3 @@
-
 type g = Unix.file_descr
 
 let block = 1
@@ -10,17 +9,14 @@ let create ?time:_ () =
   at_exit (fun () -> Unix.close fd);
   fd
 
-let rec really_read fd buf ~off len =
+let rec generate_into ~g buf ~off len =
   if len > 0 then
     try
-      let n = Unix.read fd buf off len in
+      let n = Unix.read g buf off len in
       if n = 0 then failwith "couldn't read enough bytes from /dev/urandom"
-      else really_read fd buf ~off:(off + n) (len - n)
+      else generate_into ~g buf ~off:(off + n) (len - n)
     with
-    | Unix.Unix_error (Unix.EINTR, _, _) -> really_read fd buf ~off len
-
-let generate_into ~g buf ~off len =
-  really_read g buf ~off len
+    | Unix.Unix_error (Unix.EINTR, _, _) -> generate_into ~g buf ~off len
 
 let reseed ~g:_ _data = ()
 
